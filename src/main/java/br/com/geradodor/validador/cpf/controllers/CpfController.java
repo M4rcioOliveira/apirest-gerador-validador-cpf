e675e197f8dfb674a34cf56630c4f666ea55a10e
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2021-08-16 03:42:53 -0300

    Padrão

--- src/main/java/br/com/geradodor/validador/cpf/controllers/CpfController.java
+++ src/main/java/br/com/geradodor/validador/cpf/controllers/CpfController.java
@@ -1,27 +1,49 @@
 package br.com.geradodor.validador.cpf.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.geradodor.validador.cpf.model.Cpf;
 
 @RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@RequestMapping(value = "/api")
+@CrossOrigin(origins = "*")//não usar em apis privadas
 public class CpfController {
 
+	@GetMapping("/validador")
+	@ResponseBody
+	public ResponseEntity<?> validador(@RequestParam(name = "cpf") String cpf) {
 
-	@GetMapping("/gerador/{n}")
-	public Cpf gerador(@PathVariable(value = "n") int n) {
+		Cpf cpfTrue = new Cpf(cpf, true);
+		Cpf cpfFalse = new Cpf(cpf, false);
+
+		boolean validar = Cpf.validador(cpf);
+
+		if (validar == false) {
+			return new ResponseEntity<Cpf>(cpfFalse, HttpStatus.OK);
+		}
+
+		else {
+			return new ResponseEntity<Cpf>(cpfTrue, HttpStatus.OK);
+		}
+
+	}
+
+	@GetMapping("/gerador")
+	@ResponseBody
+	public ResponseEntity<?> gerador() {
 		
 		String cpf = Cpf.gerador();
 
 		Cpf cpfGerado = new Cpf(cpf, true);
 
-		return cpfGerado;
+		return new ResponseEntity<Cpf>(cpfGerado, HttpStatus.OK);
 
 	}
 
