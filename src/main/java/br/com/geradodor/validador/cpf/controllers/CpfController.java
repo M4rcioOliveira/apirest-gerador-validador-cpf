60da8eb4509860a66b3b8e9b98dc377d854b28f5
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2021-08-16 03:28:40 -0300

    adicinado cabeçalho

--- src/main/java/br/com/geradodor/validador/cpf/controllers/CpfController.java
+++ src/main/java/br/com/geradodor/validador/cpf/controllers/CpfController.java
@@ -1,6 +1,7 @@
 package br.com.geradodor.validador.cpf.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,8 @@ import br.com.geradodor.validador.cpf.model.Cpf;
 public class CpfController {
 
 
-	@GetMapping("/gerador")
-	public Cpf gerador() {
+	@GetMapping("/gerador/{n}")
+	public Cpf gerador(@PathVariable(value = "n") int n) {
 		
 		String cpf = Cpf.gerador();
 
