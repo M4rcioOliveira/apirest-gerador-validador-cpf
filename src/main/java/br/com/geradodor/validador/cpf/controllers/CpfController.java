765bd24af147124e808e9a31c9763416e68b763e
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2021-08-16 03:37:59 -0300

    adicionei o cross origin

--- src/main/java/br/com/geradodor/validador/cpf/controllers/CpfController.java
+++ src/main/java/br/com/geradodor/validador/cpf/controllers/CpfController.java
@@ -1,5 +1,6 @@
 package br.com.geradodor.validador.cpf.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import br.com.geradodor.validador.cpf.model.Cpf;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class CpfController {
 
 
